129f9e1102f11249bd660ec8dbc80c86f4750952
Stephen Johnson <steve.eric.johnson@gmail.com>
2022-09-21 12:29:10 -0600

    initial config update

---
+++ init.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_command("syntax on")
+vim.api.nvim_command("filetype plugin indent on")
+
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.signcolumn = "number"
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.g["netrw_liststyle"] = 3
+vim.opt.guifont + "Cascadia Code PL:h12"
+
+vim.api.nvim_command("colorscheme nord")
