1a8e778806c4a20f5bdb01cfd616d4fc433ef505
Stephen Johnson <steve.eric.johnson@gmail.com>
2022-09-22 09:04:15 -0600

    add lualine plugin

--- init.lua
+++ init.lua
@@ -1,19 +1,35 @@
-vim.api.nvim_command("syntax on")
-vim.api.nvim_command("filetype plugin indent on")
+local opt = vim.opt
+local command = vim.api.nvim_command
 
-vim.opt.number = true
-vim.opt.wrap = false
-vim.opt.backup = false
-vim.opt.writebackup = false
-vim.opt.signcolumn = "number"
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
+
+command("syntax on")
+command("filetype plugin indent on")
+
+
+
+
+opt.number = true
+opt.wrap = false
+opt.backup = false
+opt.writebackup = false
+opt.signcolumn = "number"
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.termguicolors = true
+opt.background = "dark"
 vim.g["netrw_liststyle"] = 3
-vim.opt.guifont = vim.opt.guifont + "Cascadia Code PL:h12"
 
-vim.api.nvim_command("colorscheme nord")
+local fonts = {
+  "Cascadia Code PL:h12",
+  "Hack Nerd Font Mono:h12",
+  "Agave Nerd Font Mono:h13"}
+opt.guifont = fonts[3]
+
+command("colorscheme nord")
 
 vim.filetype.add({extension = {odin = 'odin'}})
+
+vim.keymap.set("t","<Esc>","<C-\\><C-n>")
+
+require('lualine').setup()
