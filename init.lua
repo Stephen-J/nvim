96237c344d4c677e57fa75af03b1d14eeff14430
Stephen Johnson <steve.eric.johnson@gmail.com>
2022-12-23 18:55:37 -0700

    update plugins

--- init.lua
+++ init.lua
@@ -1,11 +1,21 @@
-local opt = vim.opt
+local fonts = {
+  "Cascadia Code PL:h12",
+  "Hack Nerd Font Mono:h12",
+  "Agave Nerd Font Mono:h13"}
 
+vim.cmd("colorscheme nord")
 vim.cmd("syntax on")
 vim.cmd("filetype plugin indent on")
 
 
 
+vim.keymap.set("t","<Esc>","<C-\\><C-n>")
 
+
+
+vim.filetype.add({extension = {odin = 'odin'}})
+
+local opt = vim.opt
 opt.number = true
 opt.wrap = false
 opt.backup = false
@@ -17,18 +27,10 @@ opt.softtabstop = 2
 opt.termguicolors = true
 opt.background = "dark"
 vim.g["netrw_liststyle"] = 3
-
-local fonts = {
-  "Cascadia Code PL:h12",
-  "Hack Nerd Font Mono:h12",
-  "Agave Nerd Font Mono:h13"}
 opt.guifont = fonts[3]
 
-vim.cmd("colorscheme nord")
 
-vim.filetype.add({extension = {odin = 'odin'}})
 
-vim.keymap.set("t","<Esc>","<C-\\><C-n>")
 
 require('lualine').setup()
 require('workspaces').setup({
@@ -36,3 +38,21 @@ require('workspaces').setup({
                     vim.cmd('source Session.vim')
                   end}
 })
+
+require('nvim-treesitter.configs').setup {
+  highlight = {enable = true}
+}
+
+require('lspconfig').clojure_lsp.setup{}
+require('lspconfig').ols.setup{}
+require('lspconfig').elixirls.setup{
+  cmd = {'elixirls'};
+}
+
+require('lspconfig').rust_analyzer.setup{
+  cmd = {'rust-analyzer'}
+}
+
+require('lspconfig').tsserver.setup{
+  cmd = {'yarn','typescript-language-server','--stdio'}
+}
